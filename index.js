db679c22d7ff18c14cc2811fced37be1a8f2feaa
Agent Hacker <43415449+AgentHackerYT@users.noreply.github.com>
2021-11-25 16:06:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 const Discord = require("discord.js")
+const queue = new Map()
+const music = require("./utils")
 const intents = new Discord.Intents().add("DIRECT_MESSAGES").add("GUILDS").add("GUILD_MESSAGES").add("GUILD_VOICE_STATES").add("GUILD_MEMBERS")
 //console.log(intents)
-const client = new Discord.Client({intents: intents})
+const client = new Discord.Client({intents: intents , ws: {
+    properties: {
+        $browser: "$iOS"
+    }
+}})
+client.on("rateLimit" , (d) =>{
+    console.log(`Global : ${d.global}, Limit : ${d.limit} , Method : ${d.method} , Path : ${d.path} , Route : ${d.route} , Timeout : ${d.timeout}`)
+})
 client.on("ready" , () =>{
     console.log("Ready")
 })
 client.on("messageCreate" , (message , msg ,s) =>{
+    const serverQueue = queue.get(message.guild.id);
     const prefix = "lfi!"
     if (!message.content.toLowerCase().startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).trim().split(" ")
     const command = args.shift().toLowerCase()
     if(command === "play"){
-            const Player = require("./utils")
-            Player(message , args.join(" "))
+        if(!message.member.voice.channel ) return message.channel.send({content: "Join A Voice Channel"});
+        music.Player(message , args.join(" "))
+    }
+    if(command === "queue"){
+        if(!message.member.voice.channel ) return message.channel.send({content: "Join A Voice Channel"});
+        music.Queue(message.guild , message)
+    }
+    if(command === "skip"){
+        if(!message.member.voice.channel ) return message.channel.send({content: "Join A Voice Channel"});
+        music.Skip(message.guild)
+    }
+    if(command === "dc"){
+        if(!message.member.voice.channel ) return message.channel.send({content: "Join A Voice Channel"});
+        music.Disconnect(message.guild)
+    }
+    if(command === "ping"){
+        message.channel.send({content: client.ws.ping})
+
     }
 })
 client.login(TOKEN)
